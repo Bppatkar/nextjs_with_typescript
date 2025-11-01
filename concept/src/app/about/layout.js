f995d0a5192c2e4bcaea3ba9600939550a4620c2
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-11-01 15:22:40 +0530

    revising concept of app routing and nested routing

---
+++ concept/src/app/about/layout.js
@@ -0,0 +1,10 @@
+export default function AboutLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div>this is ABout layout</div>
+        {children}
+      </body>
+    </html>
+  );
+}
