0f47494e1f81c5359d4004f998005d903acdf0ff
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-11-02 14:33:25 +0530

    router group done

---
+++ concept/src/app/(admin)/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>DashBoard Page - Only Admin can see that page</div>
+  )
+}
+
+export default page
---
+++ concept/src/app/(user)/cart/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this is a Cart page and anyone/any User can see this page</div>
+  )
+}
+
+export default page
